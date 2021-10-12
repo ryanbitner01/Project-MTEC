05963a0db5ecb476d4a12c3e8f3b41c55e5d3157
ryanbitner01 <79929655+ryanbitner01@users.noreply.github.com>
2021-10-12 09:51:06 -0600

    Revert Adds

--- For Fun/FriendsWithRecipes/Recipes/AppDelegate.swift
+++ For Fun/FriendsWithRecipes/Recipes/AppDelegate.swift
@@ -9,66 +9,11 @@ import UIKit
 import Firebase
 import FirebaseStorage
 import FirebaseAuth
-<<<<<<< HEAD
-import FirebaseMessaging
-import UserNotifications
-import UserMessagingPlatform
-import GoogleMobileAds
-
-=======
->>>>>>> parent of 54122dd (Adds)
-
 let db = Firestore.firestore()
 let storage = Storage.storage()
 let auth = Auth.auth()
 
 @main
-<<<<<<< HEAD
-class AppDelegate: UIResponder, UIApplicationDelegate, MessagingDelegate, UNUserNotificationCenterDelegate {
-    
-    
-    
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        FirebaseApp.configure()
-        
-        Messaging.messaging().delegate = self
-        UNUserNotificationCenter.current().delegate = self
-        
-        // Initialize the Google Mobile Ads SDK.
-        GADMobileAds.sharedInstance().start(completionHandler: nil)
-        
-        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { success, err in
-            guard success else {return}
-            
-            
-        }
-        
-        application.registerForRemoteNotifications()
-        
-        return true
-    }
-    
-    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
-        messaging.token { token, err in
-            guard let token = token else {return}
-            print(token)
-        }
-    }
-    
-    //    func messaging(_ messaging: Messaging, didReceive remoteMessage: MessagingRemoteMessage) {
-    //        print(remoteMessage.appData)
-    //    }
-    
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // reset badge count
-        application.applicationIconBadgeNumber = 0
-    }
-    
-    // Initialize the Google Mobile Ads SDK.
-       GADMobileAds.sharedInstance().start(completionHandler: nil)    // MARK: UISceneSession Lifecycle
-    
-=======
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
@@ -81,7 +26,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     // MARK: UISceneSession Lifecycle
 
->>>>>>> parent of 54122dd (Adds)
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
