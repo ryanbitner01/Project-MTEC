74383c0c9f9a4ba6f5e745e6ef0c76d56664e437
ryanbitner01 <79929655+ryanbitner01@users.noreply.github.com>
2021-10-12 09:45:44 -0600

    Revert Adds

--- For Fun/FriendsWithRecipes/Recipes/AppDelegate.swift
+++ For Fun/FriendsWithRecipes/Recipes/AppDelegate.swift
@@ -9,17 +9,21 @@ import UIKit
 import Firebase
 import FirebaseStorage
 import FirebaseAuth
+<<<<<<< HEAD
 import FirebaseMessaging
 import UserNotifications
 import UserMessagingPlatform
 import GoogleMobileAds
 
+=======
+>>>>>>> parent of 54122dd (Adds)
 
 let db = Firestore.firestore()
 let storage = Storage.storage()
 let auth = Auth.auth()
 
 @main
+<<<<<<< HEAD
 class AppDelegate: UIResponder, UIApplicationDelegate, MessagingDelegate, UNUserNotificationCenterDelegate {
     
     
@@ -64,18 +68,32 @@ class AppDelegate: UIResponder, UIApplicationDelegate, MessagingDelegate, UNUser
     // Initialize the Google Mobile Ads SDK.
        GADMobileAds.sharedInstance().start(completionHandler: nil)    // MARK: UISceneSession Lifecycle
     
+=======
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        FirebaseApp.configure()
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+>>>>>>> parent of 54122dd (Adds)
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
-    
+
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-    
-    
+
+
 }
 
